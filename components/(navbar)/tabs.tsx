d277546d82e0ed3a316d67575f57149916b856a6
Aden Teo <88359484+adenteo@users.noreply.github.com>
2024-06-15 23:10:02 +0800

    Update navbar shadow

--- components/(navbar)/tabs.tsx
+++ components/(navbar)/tabs.tsx
@@ -1,9 +1,7 @@
 'use client';
 import Link from 'next/link';
-import { Badge } from '../ui/badge';
 import { usePathname } from 'next/navigation';
 import {
-  CirclePlus,
   HeartHandshake,
   Home,
   MessageCircle,
@@ -42,14 +40,14 @@ export default function Tabs() {
     pathname === path ? (
       <Plus
         color="white"
-        size={45}
+        size={30}
         fill="white"
         className="shadow-xl rounded-3xl"
       />
     ) : (
       <Plus
         color="white"
-        size={45}
+        size={30}
         fill="white"
         className="shadow-xl rounded-3xl"
       />
@@ -61,33 +59,29 @@ export default function Tabs() {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow-md z-50">
       <div className="max-w-screen-xl mx-auto py-4">
-        <div className="flex justify-around">
-          <Link href="/listings">
+        <div className="flex">
+          <Link href="/listings" className="flex-1 flex justify-center">
             <div className="flex flex-col items-center justify-center">
               {getHomeColor('/listings')}
-              <span
-                className={` ${getIconColor('/listings')} text-sm font-medium`}
-              >
+              <span className={` ${getIconColor('/listings')} text-sm font-medium`}>
                 Home
               </span>
             </div>
           </Link>
-          <Link href="/community">
+          <Link href="/community" className="flex-1 flex justify-center">
             <div className="flex flex-col items-center justify-center">
               {getCommunityColor('/community')}
-              <span
-                className={` ${getIconColor('/community')} text-sm font-medium`}
-              >
+              <span className={` ${getIconColor('/community')} text-sm font-medium`}>
                 Community
               </span>
             </div>
           </Link>
-          <Link href="/listings/create">
-            <div className="fixed bottom-12 left-[47%] bg-[#47A36E] rounded-3xl shadow-2xl ">
+          <Link href="/listings/create" className="flex-1 flex justify-center">
+            <div className="bg-[#47A36E] rounded-full shadow-2xl fixed bottom-8 p-4 shadow-black">
               {getCreateColor('/listings/create')}
             </div>
           </Link>
-          <Link href="/chat">
+          <Link href="/chat" className="flex-1 flex justify-center">
             <div className="flex flex-col items-center justify-center">
               {getChatColor('/chat')}
               <span className={` ${getIconColor('/chat')} text-sm font-medium`}>
@@ -95,12 +89,10 @@ export default function Tabs() {
               </span>
             </div>
           </Link>
-          <Link href="/profile">
+          <Link href="/profile" className="flex-1 flex justify-center">
             <div className="flex flex-col items-center justify-center">
               {getProfileColor('/profile')}
-              <span
-                className={` ${getIconColor('/profile')} text-sm font-medium`}
-              >
+              <span className={` ${getIconColor('/profile')} text-sm font-medium`}>
                 Profile
               </span>
             </div>
